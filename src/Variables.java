369dc4f3e768de6801f2ebdb12833ee7c2999257
Diaz armandoU <diaz.armando.u@gmail.com>
2024-10-09 20:54:02 -0600

    1Variables.java

--- src/Variables.java
+++ src/Variables.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class Variables {
+   public static void main(String[] args) {
+        //Variables
+        int edad = 31;
+        double precio = 300.4;
+        boolean disponible = true; //Verdadero o falso
+        char genero;
+        genero = 'M'; //M-Masculino o F-femanino
+
+        //acceder a las variables
+        System.out.println("Edad " + edad);
+        //Modificar el valor
+        edad = 35;
+        System.out.println("Edad " + edad);
+        
+        //Imprimir precio
+        System.out.println("Precio " + precio);
+
+        //Imprimir disponible
+        System.out.println("Exisencia " + disponible);
+
+        //imprmir genero
+        System.out.println("Genero " + genero);
+
+    }
 }
