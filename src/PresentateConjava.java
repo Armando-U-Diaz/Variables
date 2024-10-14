b1bd3b64d2d7f3e134ccdab01edc757bcbe9d0d6
Diaz ArmandoU <diaz.armando.u@gmail.com>
2024-10-13 23:19:55 -0600

    Create PresentateConjava.java

---
+++ src/PresentateConjava.java
@@ -0,0 +1,12 @@
+public class PresentateConjava {
+    public static void main(String[] args) {
+        //Imprimir mensajes
+        System.out.println("Mi nombre es Armando Uriel Diaz Matinez.");
+        System.out.println("Tengo la edad de 18 años.");
+        System.out.println("Naci en el pais de Mexico.");
+        System.out.println("Estudio en el tecnm de jilotec.");
+        System.out.println("hola");
+        System.out.println("Hola mis amigos");
+    }
+    
+}
