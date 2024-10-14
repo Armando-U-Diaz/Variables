7a92a2887e0558c61e59ddf653c9d822e3eb2b2d
Diaz ArmandoU <diaz.armando.u@gmail.com>
2024-10-13 23:06:30 -0600

    Create TiposDatos.java

---
+++ src/TiposDatos.java
@@ -0,0 +1,28 @@
+public class TiposDatos {
+    public static void main(String[] args) {
+        // Tipos de datos en Java.
+        // Enteros.
+        byte tipoByte = 127; // Valor maximo ( -128 a 127 ) del tipo Byte.
+        // tipo Byte = (byte)128 ( Para hacer la convercion) pero se perde la precision de los datos.
+        System.out.println("tipoByte = " + tipoByte); // soutv: Imprimir nombre de la variable y la variable.
+
+        short tipoShort = 32000; // Valor maximo ( -32000 a 31999 ) del tipo Short.
+        System.out.println("tipoShort = " + tipoShort);
+
+        int tipoInt = 2147483647; // Con un digito mas marca error y se tiene que hacer la convecion.
+        System.out.println("tipoInt = " + tipoInt);
+
+        long tipoLong = 987654321; // Si se exede el numero de digitos see le colocaria una letra "L" de literal.
+        System.out.println("tipoLong = " + tipoLong);
+
+
+        // Tipos de datos Punto Flotante
+        float tipoFloat = 3.24F; // Utilizamos una "F" para declarar que 3.24 es de tipo float.
+        System.out.println("tipoFloat = " + tipoFloat);
+
+        double tipoDouble = 3.1315D; // La "D" es opcional
+        System.out.println("tipoDouble = " + tipoDouble);
+        
+
+    }
+}
