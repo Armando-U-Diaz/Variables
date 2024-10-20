a342053c5bff23b3832030b3be1c382b7ecb16c0
Diaz ArmandoU <diaz.armando.u@gmail.com>
2024-10-19 21:25:58 -0600

    Create AreaVolumen.java

---
+++ src/AreaVolumen.java
@@ -0,0 +1,25 @@
+// Inicio
+import javax.swing.JOptionPane;
+
+public class AreaVolumen {
+    public static void main(String args[]) {
+        
+        // Definir
+        double r, h, area, volumen;
+        
+        // Imprimir
+        System.out.println("Calcula el area y volumen de un cilindro.");
+        
+        // Leer
+        r=Double.parseDouble(JOptionPane.showInputDialog("Ingresa el radio del cilindro."));
+        h=Double.parseDouble(JOptionPane.showInputDialog("Ingresa la altura del cilindro."));
+        
+        // Proceso
+        volumen = Math.PI * Math.pow(r, 2) * h;
+        area = (2 * Math.PI) * r;
+        
+        // Imprimir resultado
+        System.out.println("Area: " + area);
+        System.out.println("Volumen: " + volumen);
+    }
+}
