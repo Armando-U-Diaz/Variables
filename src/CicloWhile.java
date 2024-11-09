3d1befe7e403d3425caccd4b70ccb6955819567a
Diaz ArmandoU <diaz.armando.u@gmail.com>
2024-11-08 23:05:46 -0600

    Create CicloWhile.java

---
+++ src/CicloWhile.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+public class CicloWhile {
+    public static void main(String args[]) {
+        byte alum = 1;
+        float cu1, cu2, cu3, cu4, cu5, prom = 0;
+        while (alum <= 34) {
+            cu1=Float.parseFloat(JOptionPane.showInputDialog("Ingresa calificación unidad 1"));
+            cu2=Float.parseFloat(JOptionPane.showInputDialog("Ingresa calificación unidad 2"));
+            cu3=Float.parseFloat(JOptionPane.showInputDialog("Ingresa calificación unidad 3"));
+            cu4=Float.parseFloat(JOptionPane.showInputDialog("Ingresa calificación unidad 4"));
+            cu5=Float.parseFloat(JOptionPane.showInputDialog("Ingresa calificación unidad 5"));
+            prom = (cu1 + cu2 + cu3 + cu4 + cu5) / 5;
+            System.out.println("Alumno: " + alum);
+            System.out.println("Promedio: " + prom);
+            alum ++;
+        }
+    }
+}
